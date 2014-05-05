ec259c576dec1c64b0d83bc8eabda3348439d587
twovl <wangliang2000@hotmail.com>
2014-05-05 21:43:24 +0800

    delete no use file

--- deviceInfo.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by 亮 on 2014/4/29.
- */
-exports.udid = "db2ca54b9751ac55e800c41e8a4ed91d";
-exports.gameName="CARD-WP8-CHS";
-exports.clientType= "wp8";
-exports.releaseChannel= "wp8"
-exports.locale= "ZH_CN";
