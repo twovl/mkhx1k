3fcd66e103c534023c9906998815cf5aca1896b3
twovl <wangliang2000@hotmail.com>
2014-06-16 15:46:34 +0800

    Signed-off-by: twovl <wangliang2000@hotmail.com>

--- routes/maze.js
+++ routes/maze.js
@@ -4,24 +4,13 @@ exports.show = function (req,res){
     var mapStageId = req.params.mapStageId;
     var remoteInfo = req.cookies.remoteInfo;
     mkhxServer.maze.show(remoteInfo.host, remoteInfo.sid, mapStageId, function (err, mazeInfo){
-        if(err){
-            res.send(JSON.stringify(err));
-        }
-        else{
-            res.send(JSON.stringify(mazeInfo));
-        }
+        res.send(JSON.stringify(err ? err : mazeInfo));
     });
 };
 exports.infos = function(req, res){
     var mapStageId = req.params.mapStageId;
     var remoteInfo = req.cookies.remoteInfo;
     mkhxServer.maze.infos(remoteInfo.host, remoteInfo.sid, mapStageId, function (err, mazeInfos){
-        if(err){
-            res.send(JSON.stringify(err));
-        }
-        else{
-
-            res.send(JSON.stringify(mazeInfos));
-        }
+        res.send(JSON.stringify(err ? err : mazeInfos));
     });
 };
